682a97cb7ec7adff4751fe5b34f83be60f8b5698
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-25 13:15:47 +0100

    Chiamata ajax: stampo in console i 4 dati che mi interessano (prova)

---
+++ myscript.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+  $.ajax ({
+    url: "https://api.themoviedb.org/3/movie/550?api_key=26b65514bf0d0d8d8b3921ff50e0770b&query=ritorno+al+futuro",
+    method: "GET",
+    success: function (filmapi) {
+      var title = filmapi.title;
+      console.log("il titolo è:", title);
+      var original_title = filmapi.original_title;
+      console.log("il titolo originale è:", original_title);
+      var original_language = filmapi.original_language;
+      console.log("la lingua originale è:", original_language);
+      var vote_average = filmapi.vote_average;
+      console.log("il voto è:", vote_average);
+    }
+  })
+
+  var template = Handlebars.compile("{{doesWhat}}");
+  // execute the compiled template and print the output to the console
+  console.log(template({ doesWhat: "rocks!" }));
+
+
+
+});
