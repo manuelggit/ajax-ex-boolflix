37245c4acf07e242d76bb540f4568a7600c50631
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-25 17:57:22 +0100

    Solo al click mi appaiono i risultati in console

--- myscript.js
+++ myscript.js
@@ -1,30 +1,38 @@
 $(document).ready(function(){
 
-$('input').keyup(function(event){
+  $('input').keyup(function(event){
 
-  var query = $('input').val().toLowerCase();
-  console.log(query);
+    var query = $('input').val().toLowerCase();
+    console.log(query);
 
-  $.ajax ({
-    url: "https://api.themoviedb.org/3/search/movie?api_key=26b65514bf0d0d8d8b3921ff50e0770b",
-    method: "GET",
-    data: {
-      query: query
-    },
-    success: function (filmapi) {
-      console.log(filmapi);
+  $('button').click(function(){
 
-      var listaFilm = filmapi.results;
-      console.log(listaFilm);
+    $.ajax ({
+      url: "https://api.themoviedb.org/3/search/movie?api_key=26b65514bf0d0d8d8b3921ff50e0770b",
+      method: "GET",
+      data: {
+        query: query
+      },
+      success: function (filmapi) {
+        console.log(filmapi);
 
-      for (var i = 0; i < listaFilm.length; i++){
-        var titoloOriginale = listaFilm[i].original_title;
-        console.log(titoloOriginale);
+        var listaFilm = filmapi.results;
+        console.log(listaFilm);
+
+        for (var i = 0; i < listaFilm.length; i++){
+          var titoloOriginale = listaFilm[i].original_title;
+          console.log(titoloOriginale);
+        }
+
       }
 
-    }
+    });
 
   });
+
+
+
+
 
 })
 
