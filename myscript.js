7759807e0eb609cbd0805700bb1d95b4c667f57d
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-27 11:54:30 +0100

    Correzione: chiamata ajax per serie tv + aggiunta del "tipo" nella funzione stampa

--- myscript.js
+++ myscript.js
@@ -9,30 +9,73 @@ $(document).ready(function(){
   // al click sul "button"
   $('button').click(function(){
 
-    // faccio chiamata ajax
+    // faccio chiamata ajax movie
     $.ajax ({
       url: "https://api.themoviedb.org/3/search/movie?api_key=26b65514bf0d0d8d8b3921ff50e0770b",
       method: "GET",
       data: {
         query: query, // cambio dinamicamente l'url con la ricerca dell'utente
-        original_language: "it-IT", //cerco solo i risultati in italiano
+        original_language: "it-IT", //cerco solo i risultati-film in italiano
       },
 
-      success: function (filmapi) {
-        // console.log(filmapi); // stampo tutto
+      success: function (data) {
+        // console.log(data); // stampo tutto
         // // stampo a console tutti gli oggetti
-        var listaFilm = filmapi.results; // stampo solo l'array di oggetti "results"
+        var listaFilm = data.results; // stampo solo l'array di oggetti "results"
         stampa(listaFilm); //invoco la funzione per stampare la lista dei film
-        if (listaFilm.length > 0) { //rendo l'input vuoto solo se ho ricevuto dei risultati
+        if (listaFilm.length > 0) { //rendo l'input vuoto solo se ho ricevuto dei risultati-film
           inputReset(); // invoco la funzione di inputReset
         }
       },
 
       error: function(error){
-        console.log('Errore', error);
+        // console.log('Errore', error);
       }
 
     });
+
+    // faccio chiamata ajax tv
+    $.ajax ({
+      url: "https://api.themoviedb.org/3/search/tv?api_key=26b65514bf0d0d8d8b3921ff50e0770b",
+      method: "GET",
+      data: {
+        query: query, // cambio dinamicamente l'url con la ricerca dell'utente
+        original_language: "it-IT", //cerco solo i risultati-film in italiano
+      },
+
+      success: function (data) {
+        // console.log(data); // stampo tutto
+        // // stampo a console tutti gli oggetti
+        var listaFilm = data.results; // stampo solo l'array di oggetti "results"
+        stampa(listaFilm); //invoco la funzione per stampare la lista dei film
+        if (listaFilm.length > 0) { //rendo l'input vuoto solo se ho ricevuto dei risultati-film
+          inputReset(); // invoco la funzione di inputReset
+        }
+      },
+
+      error: function(error){
+        // console.log('Errore', error);
+      }
+
+    });
+
+    // $.ajax({
+    //   url: "https://api.themoviedb.org/3/search/tv?api_key=26b65514bf0d0d8d8b3921ff50e0770b",
+    //   method: "GET",
+    //   data: {
+    //     query: query, // cambio dinamicamente l'url con la ricerca dell'utente
+    //     original_language: "it-IT", //cerco solo i risultati-film in italiano
+    //   },
+    //   success: function(serieTvapi){
+    //     // console.log(serieTvapi);
+    //     var listaSerieTv = serieTvapi.results;
+    //     console.log(listaSerieTv);
+    //     stampa(listaSerieTv);
+    //     if (listaSerieTv.length > 0) {
+    //       inputReset();
+    //     }
+    //   }
+    // });
 
   });
 
@@ -48,8 +91,8 @@ function inputReset(){
 }
 
 // funzione per stampare la lista dei film
-function stampa(listaFilm){
-  var risultati = $('#risultati');
+function stampa(tipo, listaFilm){
+  var risultatiFilm = $('#risultati-film');
 
   var sorgenteHtml = $('#film-template').html();
   var template = Handlebars.compile(sorgenteHtml);
@@ -60,51 +103,116 @@ function stampa(listaFilm){
     var context = { //compiliamo il context con ciò che ci serve
       Titolo: film.title,
       Titolo_originale: film.original_title,
-      Lingua: stampaBandiere(),
+      Lingua: film.original_language,
       Voto: film.vote_average,
-      Stelle: stampaStelle(),
     }
 
 
     var html = template(context); //estraiamo l'html dal context compilato
-    risultati.append(html); //appendiamolo all'interno di "#risultati"
+    risultatiFilm.append(html); //appendiamolo all'interno di "#risultati-film"
   }
 
-  // funzione per stampare le stelle
-  function stampaStelle(stella){
-    // dimezzo e arrotondo il voto in funzione della creazione delle stelline
-    var voto = film.vote_average;
-    var stelle = Math.round(voto/2);
-    console.log('stelle del film', stelle);
+  // // funzione per stampare le stelle
+  // function stampaStelle(stella){
+  //   // dimezzo e arrotondo il voto in funzione della creazione delle stelline
+  //   var voto = film.vote_average;
+  //   var stelle = Math.round(voto/2);
+  //   // console.log('stelle del film', stelle);
+  //
+  //   var contenitoreStelle = [];
+  //   // console.log(contenitoreStelle);
+  //   for (var i = 1; i <= 5; i++){
+  //     if(i <= stelle){
+  //       // console.log(i, 'stella piena');
+  //       // contenitoreStelle.push('<i class="fas fa-star"></i>');
+  //     } else {
+  //       // console.log(i, 'stella vuota');
+  //       // contenitoreStelle.push('<i class="far fa-star"></i>');
+  //     }
+  //   return contenitoreStelle
+  //
+  //   }
+  // }
 
-    var contenitoreStelle = [];
-    console.log(contenitoreStelle);
-    for (var i = 1; i <= 5; i++){
-      if(i <= stelle){
-        console.log(i, 'stella piena');
-        // contenitoreStelle.push('<i class="fas fa-star"></i>');
-      } else {
-        console.log(i, 'stella vuota');
-        // contenitoreStelle.push('<i class="far fa-star"></i>');
-      }
-    return contenitoreStelle
-
-    }
-  }
-
-  function stampaBandiere(){
-    var aggiungiBandiera = '';
-    var lingua = film.original_language;
-    console.log(lingua);
-    if (lingua === 'it'){
-      aggiungiBandiera = '<img src="img/it.png" alt="italia">'
-    } else if (lingua === 'en'){
-      aggiungiBandiera = '<img src="img/en.png" alt="inghilterra">'
-    } else {
-      aggiungiBandiera = lingua;
-    }
-    return aggiungiBandiera;
-  }
+  // // funzione per stampare le bandiere
+  // function stampaBandiere(){
+  //   var aggiungiBandiera = '';
+  //   var lingua = film.original_language;
+  //   // console.log(lingua);
+  //   if (lingua === 'it'){
+  //     aggiungiBandiera = '<img src="img/it.png" alt="italia">'
+  //   } else if (lingua === 'en'){
+  //     aggiungiBandiera = '<img src="img/en.png" alt="inghilterra">'
+  //   } else {
+  //     aggiungiBandiera = lingua;
+  //   }
+  //   return aggiungiBandiera;
+  // }
 
 
 }
+
+// // Serietv
+//
+// // funzione per stampare la lista delle serieTv
+// function stampa(listaSerieTv){
+//   var risultatiSerieTv = $('#risultati-serietv');
+//
+//   var sorgenteHtml = $('#film-template').html();
+//   var template = Handlebars.compile(sorgenteHtml);
+//
+//   for (var j = 0; j < listaSerieTv.length; j++) { //ciclo la lista delle serie tv
+//     var serieTv = listaSerieTv[j]; //mi ricavo le singole serie tv
+//
+//     var contextSerieTv = { //compiliamo il context con ciò che ci serve
+//       Titolo: serieTv.title,
+//       Titolo_originale: serieTv.original_title,
+//       Lingua: stampaBandiereSerieTV(),
+//       Voto: serieTv.vote_average,
+//       Stelle: stampaStelleSerieTV(),
+//     }
+//
+//
+//     var htmlSerieTV = template(contextSerieTv); //estraiamo l'html dal context compilato
+//     risultatiSerieTv.append(htmlSerieTV); //appendiamolo all'interno di "#risultati-serietv"
+//   }
+//
+//   // funzione per stampare le stelle
+//   function stampaStelleSerieTV(stelleSerieTV){
+//     // dimezzo e arrotondo il voto in funzione della creazione delle stelline
+//     var votoSerieTV = serieTv.vote_average;
+//     var stelleSerieTV = Math.round(votoSerieTV/2);
+//     // console.log('stelle del film', stelle);
+//
+//     var contenitoreStelleSerieTV = [];
+//     // console.log(contenitoreStelle);
+//     for (var j = 1; j <= 5; j++){
+//       if(j <= stelleSerieTV){
+//         // console.log(i, 'stella piena');
+//         // contenitoreStelle.push('<i class="fas fa-star"></i>');
+//       } else {
+//         // console.log(i, 'stella vuota');
+//         // contenitoreStelle.push('<i class="far fa-star"></i>');
+//       }
+//     return contenitoreStelleSerieTV
+//
+//     }
+//   }
+//
+//   // funzione per stampare le bandiere
+//   function stampaBandiereSerieTV(){
+//     var aggiungiBandieraSerieTV = '';
+//     var linguaSerieTV = serieTv.original_language;
+//     // console.log(lingua);
+//     if (linguaSerieTV === 'it'){
+//       aggiungiBandieraSerieTV = '<img src="img/it.png" alt="italia">'
+//     } else if (linguaSerieTV === 'en'){
+//       aggiungiBandieraSerieTV = '<img src="img/en.png" alt="inghilterra">'
+//     } else {
+//       aggiungiBandieraSerieTV = linguaSerieTV;
+//     }
+//     return aggiungiBandieraSerieTV;
+//   }
+//
+//
+// }
